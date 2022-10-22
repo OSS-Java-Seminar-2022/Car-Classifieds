115364525071c0773d90976f48186e295164a438
Josip <jcjurkic@gmail.com>
2022-10-22 18:29:54 +0200

    Initial configuration

---
+++ src/main/java/User.java
@@ -0,0 +1,20 @@
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userId;
+    @Column(name = "user_type_id")
+    private int userTypeId;
+    @Column(name = "user_data_id")
+    private int userDataId;
+    @Column(name = "user_credentials_id")
+    private int userCredentialsId;
+
+}
+
+
