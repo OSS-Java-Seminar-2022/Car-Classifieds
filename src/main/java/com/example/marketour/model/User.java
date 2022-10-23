d90d79e2cc71b76f84697a54499269203ccaf070
JakovSoldic <jakovsoldic123@gmail.com>
2022-10-23 19:46:31 +0200

    Flyway config

--- src/main/java/com/example/marketour/model/User.java
+++ src/main/java/com/example/marketour/model/User.java
@@ -3,15 +3,16 @@ package com.example.marketour.model;
 import javax.persistence.*;
 
 @Entity
+@Table(name = "user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private int userId;
-    @Column(name = "user_type_id")
-    private int userTypeId;
+    @OneToOne(mappedBy = "user",cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private UserType userType;
     @Column(name = "user_data_id")
     private int userDataId;
     @Column(name = "user_credentials_id")
     private int userCredentialsId;
-
 }
--- src/main/java/com/example/marketour/model/UserType.java
+++ src/main/java/com/example/marketour/model/UserType.java
@@ -3,10 +3,14 @@ package com.example.marketour.model;
 import javax.persistence.*;
 
 @Entity
+@Table(name = "userType")
 public class UserType {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int userTypeId;
     @Column(name = "name")
     private String name;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 }
