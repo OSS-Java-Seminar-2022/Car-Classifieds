5af9916c1e03f6eaa5ee20f714df52e0baeff227
Antonio Sabic <as@evolutio.hr>
2022-12-17 13:13:04 +0100

    added all other repos

---
+++ src/main/java/com/example/marketour/repositories/TourReviewRepository.java
@@ -0,0 +1,10 @@
+package com.example.marketour.repositories;
+
+import com.example.marketour.model.TourReview;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TourReviewRepository extends JpaRepository<TourReview, Long> {
+
+}
