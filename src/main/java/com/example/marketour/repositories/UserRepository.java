3edb66f775b04cd6ca48954d035eb1959baeff17
Antonio Sabic <as@evolutio.hr>
2022-12-26 20:01:47 +0100

    removed

--- src/main/java/com/example/marketour/repositories/UserRepository.java
+++ src/main/java/com/example/marketour/repositories/UserRepository.java
@@ -6,6 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    /*@Query("SELECT u FROM User u WHERE u.userCredentials.username = (:username) AND u.userCredentials.password = (:password)")
-    User findWithUsernameAndPassword(@Param("username") String username, @Param("password") String password);*/
 }
