9730f0f61116d3582395e822644f2626630dd55f
Antonio Sabic <as@evolutio.hr>
2023-01-14 16:34:42 +0100

    images work

--- src/main/java/com/example/marketour/controllers/RouteController.java
+++ src/main/java/com/example/marketour/controllers/RouteController.java
@@ -1,18 +1,29 @@
 package com.example.marketour.controllers;
 
-import com.example.marketour.model.entities.City;
-import com.example.marketour.model.entities.Country;
-import com.example.marketour.model.entities.User;
-import com.example.marketour.model.entities.UserType;
+import com.example.marketour.model.entities.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 @Controller
 @RequestMapping
 public class RouteController {
+    private final TourController tourController;
+    private final ImageController imageController;
+
+    public RouteController(TourController tourController, ImageController imageController) {
+        this.tourController = tourController;
+        this.imageController = imageController;
+    }
 
     @GetMapping("/")
     public String indexPage() {
@@ -33,7 +44,23 @@ public class RouteController {
     }
 
     @GetMapping(value = "/main")
-    String main(@ModelAttribute("user") User user, Model model) {
+    String main(@ModelAttribute("user") User user, HttpServletRequest httpServletRequest, Model model) {
+        final var result = tourController.getAllToursOfThisUser(httpServletRequest, model);
+        if (result.getBody() instanceof ArrayList) {
+            Map<Long, Image> tourIdToImageMap = new HashMap<>();
+            if (!((ArrayList<?>) result.getBody()).isEmpty() && ((ArrayList<?>) result.getBody()).get(0) instanceof TouristTour) {
+                tourIdToImageMap = ((ArrayList<TouristTour>) result.getBody()).stream().map(touristTour -> Map.entry(touristTour.getTour().getTourId(), imageController.getTourImages(touristTour.getTour().getTourId()).getBody().get(0))).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+            } else if (!((ArrayList<?>) result.getBody()).isEmpty() && ((ArrayList<?>) result.getBody()).get(0) instanceof GuideTour) {
+                tourIdToImageMap = ((ArrayList<GuideTour>) result.getBody()).stream().map(guideTour -> Map.entry(guideTour.getTour().getTourId(), imageController.getTourImages(guideTour.getTour().getTourId()).getBody().get(0))).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+            }
+            final var finalMap = tourIdToImageMap.entrySet().stream().map(entry -> Map.entry(entry.getKey(), Base64.getEncoder().encodeToString(entry.getValue().getData()))).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+            model.addAttribute("userTours", result.getBody());
+            model.addAttribute("imageMap", finalMap);
+        } else {
+            //TODO Error handling
+        }
+
+
         return "main";
     }
 
